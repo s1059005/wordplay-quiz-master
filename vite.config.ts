613127913a271b9edc2f55c49fe4290de42a23fa
smallken99 <30430431+smallken99@users.noreply.github.com>
2026-03-22 11:43:19 +0800

    Configure custom domain wordplay.smallken.site

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/wordplay-quiz-master/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
