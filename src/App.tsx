293f2467990fbf68d74bcad8a6369e75457f6e28
smallken99 <30430431+smallken99@users.noreply.github.com>
2026-03-22 11:47:06 +0800

    Fix React Router basename for custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/wordplay-quiz-master/">
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
